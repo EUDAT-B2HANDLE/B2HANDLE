84db0c48a69ce93fcbfca59cf4bb6146a41d21d4
merret <buurman@dkrz.de>
2015-08-26 21:33:23 +0200

    Added tests for those methods that do not need any read/write access to any server.

--- b2handle/handleclient_test.py
+++ b2handle/handleclient_test.py
@@ -1,36 +1,42 @@
+'''
+Testing methods that need no server access.
+'''
+
 import unittest
-import handleclient
+import b2handle.handleclient as b2handle
+from b2handle.handleexceptions import HandleSyntaxError
 
 class EUDATHandleClient_test(unittest.TestCase):
 
-    # So far, we only test if the module is loaded correctly and if the methods can be called using the defined signatures and (numbers of) arguments.
-
     def setUp(self):
-        self.client = handleclient.EUDATHandleClient()
+        self.inst = b2handle.EUDATHandleClient()
 
-    def tearDown(self):
-        pass
+    def generate_PID_name_test(self):
+        uuid = self.inst.generate_PID_name()
+        assert '/' not in uuid
+        uuid = self.inst.generate_PID_name('aprefix')
+        assert 'aprefix/' in uuid
 
-    def init_test_1(self):
-        dummy = handleclient.EUDATHandleClient()
+    def init_test(self):
+        b2handle.EUDATHandleClient()
+        b2handle.EUDATHandleClient('foo')
+        b2handle.EUDATHandleClient('http://foo.bar')
 
-    def init_test_2(self):
-        dummy = handleclient.EUDATHandleClient("https://hdl.handle.net")
+    def check_handle_syntax_test(self):
+        syntax_checked = self.inst.check_handle_syntax("foo/bar")
+        assert syntax_checked == True
+        # Exceptions if too many slashes, too few slashes, no prefix, no suffix:
+        self.assertRaises(HandleSyntaxError, self.inst.check_handle_syntax, "foo/bar/foo")
+        self.assertRaises(HandleSyntaxError, self.inst.check_handle_syntax, "foobar")
+        self.assertRaises(HandleSyntaxError, self.inst.check_handle_syntax, "/bar")
+        self.assertRaises(HandleSyntaxError, self.inst.check_handle_syntax, "foo/")
 
-    def init_test_3(self):
-        dummy = handleclient.EUDATHandleClient("xyz")
+    def check_handle_syntax_with_index_test(self):
+        syntax_checked = self.inst.check_handle_syntax("300:foo/bar")
+        assert syntax_checked == True
+        # Exceptions if no index, index wrong, no handle, several indices:
+        self.assertRaises(HandleSyntaxError, self.inst.check_handle_syntax_with_index, "nonumber:foo/bar")
+        self.assertRaises(HandleSyntaxError, self.inst.check_handle_syntax_with_index, "index/missing")
+        self.assertRaises(HandleSyntaxError, self.inst.check_handle_syntax_with_index, "too:many:colons")
+        self.assertRaises(HandleSyntaxError, self.inst.check_handle_syntax_with_index, "onlyindex:")
 
-    def instantiateWithUsernamePassword_test(self):
-        dummy = handleclient.EUDATHandleClient.instantiateWithUsernamePassword("xyz", "userbla", "passwordbla")
-
-    def instantiateWithCredentials_test(self):
-        credentials = "this is a credentials object..."
-        dummy = handleclient.EUDATHandleClient.instantiateWithCredentials(credentials)
-
-    def dummy_test(self):
-        dummyresult = self.client.dummy()
-        assert dummyresult==True
-
-
-
-        
