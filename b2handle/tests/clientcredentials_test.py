b56299e008f38c26883f4ade73e41e35d183ba74
merret <buurman@dkrz.de>
2015-09-14 18:22:21 +0200

    Cleaned up unit tests for client credentials (imports, split up long tests into several, better naming, ...)

--- b2handle/tests/clientcredentials_test.py
+++ b2handle/tests/clientcredentials_test.py
@@ -1,40 +1,71 @@
 import unittest
 from b2handle.clientcredentials import PIDClientCredentials
-from b2handle.handleexceptions import * 
-import mock
-import requests
-import urllib
+from b2handle.handleexceptions import HandleSyntaxError
+from b2handle.handleexceptions import CredentialsFormatError
 import json
-import xml.etree.ElementTree as ET
 
-TESTVALUES = json.load(open('tests/resources/testvalues_for_clientcredentials_tests_PUBLIC.json'))
+PATH_RES = 'tests/resources'
+PATH_CRED = 'tests/testcredentials'
+TESTVALUES = json.load(
+    open(PATH_RES+'/testvalues_for_clientcredentials_tests_PUBLIC.json'))
 
 
-class PIDClientCredentials_noaccess_test(unittest.TestCase):
+class PIDClientCredentials_test(unittest.TestCase):
+    """Test case for the PIDClientCredentials class.
+    No access to any Handle Server needed."""
 
-    def init_test(self):
-        randompassword='apsodasdjuhfsikjdfskdfj'
-        # No exception if password wrong or if server url is wrong, or if user does not exist:
-        credentials = PIDClientCredentials(TESTVALUES['url'], TESTVALUES['user'], randompassword)
-        credentials = PIDClientCredentials(TESTVALUES['url'], '100:not/exist', randompassword)
-        credentials = PIDClientCredentials('blablabla',  TESTVALUES['user'], randompassword)
-        # Exception if user is not index:prefix/suffix
-        self.assertRaises(HandleSyntaxError, PIDClientCredentials, TESTVALUES['url'], TESTVALUES['handle'], randompassword)
-        # 
+    def test_credentials_constructor(self):
+        """Test credentials instantiation. No exceptions occur if password or
+        serverurl are wrong, or if the username is inexistent. """
+        inst = PIDClientCredentials(TESTVALUES['url'],
+                                    TESTVALUES['user'],
+                                    'some_random_pasword_aksdfadjhfbsdf')
+        inst = PIDClientCredentials(TESTVALUES['url'],
+                                    '100:not/exist',
+                                    'some_random_pasword_aksdfadjhfbsdf')
+        inst = PIDClientCredentials('blablabla',
+                                    TESTVALUES['user'],
+                                    'some_random_pasword_aksdfadjhfbsdf')
 
-    def load_from_JSON_test(self):
-        pathToJsonCredentials = 'tests/testcredentials/credentials_correct_PUBLIC.json'
-        inst = PIDClientCredentials.load_from_JSON(pathToJsonCredentials)
-        # Exception if user name is not well formatted:
-        pathToJsonCredentials = 'tests/testcredentials/credentials_noindex_PUBLIC.json'
-        self.assertRaises(HandleSyntaxError, PIDClientCredentials.load_from_JSON,pathToJsonCredentials)
-        pathToJsonCredentials = 'tests/testcredentials/credentials_wrongusername_PUBLIC.json'
-        self.assertRaises(HandleSyntaxError, PIDClientCredentials.load_from_JSON,pathToJsonCredentials)
-        # Exception if items are missing:
-        pathToJsonCredentials = 'tests/testcredentials/credentials_usernamemissing_PUBLIC.json'
-        self.assertRaises(CredentialsFormatError, PIDClientCredentials.load_from_JSON,pathToJsonCredentials)
-        # Exception if items are empty:
-        pathToJsonCredentials = 'tests/testcredentials/credentials_usernameempty_PUBLIC.json'
-        self.assertRaises(CredentialsFormatError, PIDClientCredentials.load_from_JSON, pathToJsonCredentials)
+    def test_credentials_invalid_username(self):
+        """Test error when user name is not index:prefix/suffix."""
+        self.assertRaises(HandleSyntaxError, PIDClientCredentials,
+                          TESTVALUES['url'],
+                          TESTVALUES['handle'],
+                          'some_random_pasword_aksdfadjhfbsdf')
+
+    def test_credentials_from_json(self):
+        """Test credentials instantiation from JSON file."""
+        path_to_json_credentials = PATH_CRED+'/credentials_correct_PUBLIC.json'
+        inst = PIDClientCredentials.load_from_JSON(path_to_json_credentials)
+
+    def test_credentials_from_json_username_without_index(self):
+        """Test error when user name in json file does not have an index."""
+        path_to_json_credentials = PATH_CRED+'/credentials_noindex_PUBLIC.json'
+        self.assertRaises(HandleSyntaxError,
+                          PIDClientCredentials.load_from_JSON,
+                          path_to_json_credentials)
+
+    def test_credentials_from_json_invalid_username(self):
+        """Test error when user name in json file is not index:prefix/suffix."""
+        path_to_json_credentials = PATH_CRED+'/credentials_wrongusername_PUBLIC.json'
+        self.assertRaises(HandleSyntaxError,
+                          PIDClientCredentials.load_from_JSON,
+                          path_to_json_credentials)
+
+    def test_credentials_from_json_missing_items(self):
+        """Test error when items are missing from json credentials file."""
+        path_to_json_credentials = PATH_CRED+'/credentials_usernamemissing_PUBLIC.json'
+        self.assertRaises(CredentialsFormatError,
+                          PIDClientCredentials.load_from_JSON,
+                          path_to_json_credentials)
+
+
+    def test_credentials_from_json_empty_items(self):
+        """Test error when items are empty in json credentials file."""
+        path_to_json_credentials = PATH_CRED+'/credentials_usernameempty_PUBLIC.json'
+        self.assertRaises(CredentialsFormatError,
+                          PIDClientCredentials.load_from_JSON,
+                          path_to_json_credentials)
 
 
