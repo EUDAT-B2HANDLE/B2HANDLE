50f88e3b932de26c0b4da3756370654ff321ea6f
merret <buurman@dkrz.de>
2016-02-09 19:46:20 +0100

    Improved documentation of util module.

--- b2handle/util.py
+++ b2handle/util.py
@@ -1,3 +1,9 @@
+'''
+This module provides some functions that are
+    needed across various modules of the
+    b2handle library.
+'''
+
 import logging
 import handleexceptions
 import urllib
@@ -5,23 +11,40 @@ import base64
 
 
 class NullHandler(logging.Handler):
+    '''
+    A NullHandler is used to be able to define loggers
+        in library modules without logging to any target.
+        The library user then defines the logging target by
+        adding own Handler instances to the root logger.
+
+    Please see the documentation of the logging module
+        for help on logging.
+
+    The NullHandler class is required when the library
+        is run using Python 2.6. After this, the logging
+        module contains a class NullHandler to use.
+    '''
     def emit(self, record):
         pass
 
-h = NullHandler()
-
 LOGGER = logging.getLogger(__name__)
-LOGGER.addHandler(h)
+LOGGER.addHandler(NullHandler())
 
 def add_missing_optional_args_with_value_none(args, optional_args):
-    if not type(args) == 'dict' and not type(optional_args)==list:
-        if type(args) == list and type(optional_args) == dict:
-            temp = optional_args
-            optional_args = args
-            args = temp
-        else:
-            raise ValueError('Wrong argument types for method "add_missing_optional_args_with_value_none"')
-        
+    '''
+    Adds key-value pairs to the passed dictionary, so that
+        afterwards, the dictionary can be used without needing
+        to check for KeyErrors.
+
+    If the keys passed as a second argument are not present,
+        they are added with None as a value.
+
+    :args: The dictionary to be completed.
+    :optional_args: The keys that need to be added, if
+        they are not present.
+    :return: The modified dictionary.
+    '''
+      
     for name in optional_args:
         if not name in args.keys():
             args[name] = None
@@ -31,12 +54,10 @@ def remove_index_from_handle(handle_with_index):
     '''
     Returns index and handle separately, in a tuple.
 
-    :param handle_with_index: The handle string with an index (e.g.
+    :handle_with_index: The handle string with an index (e.g.
         500:prefix/suffix)
     :return: index and handle as a tuple.
     '''
-
-    LOGGER.debug('remove_index...')
 
     split = handle_with_index.split(':')
     if len(split) == 2:
@@ -51,16 +72,14 @@ def remove_index_from_handle(handle_with_index):
             
 def check_handle_syntax(string):
     '''
-    Checks the syntax of a handle without an index (are prefix and suffix
-    there, are there too many slashes).
+    Checks the syntax of a handle without an index (are prefix
+        and suffix there, are there too many slashes?).
 
     :string: The handle without index, as string prefix/suffix.
     :raise: :exc:`~b2handle.handleexceptions.handleexceptions.HandleSyntaxError`
     :return: True. If it's not ok, exceptions are raised.
 
     '''
-
-    LOGGER.debug('check_handle_syntax...')
 
     expected = 'prefix/suffix'
 
@@ -92,14 +111,12 @@ def check_handle_syntax(string):
 def check_handle_syntax_with_index(string, base_already_checked=False):
     '''
     Checks the syntax of a handle with an index (is index there, is it an
-    integer), and of the handle itself.
+        integer?), and of the handle itself.
     
     :string: The handle with index, as string index:prefix/suffix.
     :raise: :exc:`~b2handle.handleexceptions.handleexceptions.HandleSyntaxError`
     :return: True. If it's not ok, exceptions are raised.
     '''
-
-    LOGGER.debug('check_handle_syntax_with_index...')
 
     expected = 'index:prefix/suffix'
     try:
@@ -125,14 +142,12 @@ def check_handle_syntax_with_index(string, base_already_checked=False):
 
 def create_authentication_string(username, password):
     '''
-    Create an authentication string from the username and password.
+    Creates an authentication string from the username and password.
 
-    :param username: Username.
-    :param password: Password.
+    :username: Username.
+    :password: Password.
     :return: The encoded string.
     '''
-
-    LOGGER.debug('create_authentication_string...')
 
     username_utf8 = username.encode('utf-8')
     userpw_utf8 = password.encode('utf-8')
@@ -144,9 +159,29 @@ def create_authentication_string(username, password):
     return authinfostring_base64
 
 def make_request_log_message(**args):
+    '''
+    Creates a string containing all relevant information
+        about a request made to the Handle System, for
+        logging purposes.
+
+    :handle: The handle that the request is about.
+    :url: The url the request is sent to.
+    :headers: The headers sent along with the request.
+    :verify: Boolean parameter passed to the requests
+        module (https verification).
+    :resp: The request's response.
+    :op: The library operation during which the request
+        was sent.
+    :payload: Optional. The payload sent with the request.
+    :return: A formatted string.
+
+    '''
 
     mandatory_args = ['op', 'handle', 'url', 'headers', 'verify', 'resp']
+    optional_args = ['payload']
     check_presence_of_mandatory_args(args, mandatory_args)
+    add_missing_optional_args_with_value_none(args, optional_args)
+
     space = '\n   '
     message = ''
     message += '\n'+args['op']+' '+args['handle']
@@ -160,6 +195,21 @@ def make_request_log_message(**args):
     return message
 
 def check_presence_of_mandatory_args(args, mandatory_args):
+    '''
+    Checks whether all mandatory arguments are passed.
+
+    This function aims at methods with many arguments
+        which are passed as kwargs so that the order
+        in which the are passed does not matter.
+
+    :args: The dictionary passed as args.
+    :mandatory_args: A list of keys that have to be
+        present in the dictionary.
+    :raise: :exc:`~ValueError`
+    :returns: True, if all mandatory args are passed. If not,
+        an exception is raised.
+
+    '''
     missing_args = []
     for name in mandatory_args:
         if name not in args.keys():
@@ -170,8 +220,22 @@ def check_presence_of_mandatory_args(args, mandatory_args):
         return True
 
 def string_to_bool(string):
+    '''
+    Parses a string to a boolean. Accepts the words
+        "true" and "false" in any mixture of capital
+        andnon-capital letters. If the word is neither
+        "true" nor "false", a KeyError is raised.
+
+    :string: The string to parse. Passing a boolean
+        does not harm.
+    :returns: True or False.
+    :raise: :exc:`~KeyError`
+    '''
+
     dic = {'false':False, 'true':True}
     if string is True or string is False:
         return string
     else:
         return dic[string.lower()]
+
+
