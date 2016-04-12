1877eb7b9269e2c11f7f69d12d8b2d6cdaed3aa6
Javier Aguirre <contacto@javaguirre.net>
2016-04-12 10:10:42 +0200

    Changed utilconfig_test utilconfig import

--- b2handle/tests/utilconfig_test.py
+++ b2handle/tests/utilconfig_test.py
@@ -4,26 +4,23 @@ if sys.version_info < (2, 7):
 else:
     import unittest
 
-from b2handle import utilconfig
+from b2handle.utilconfig import get_valid_https_verify
 
 
 class UtilConfigTestCase(unittest.TestCase):
 
     def test_valid_https_verify_bool_true(self):
         """Test return bool True when getting bool True"""
-        self.assertEqual(utilconfig.get_valid_https_verify(True), True)
+        self.assertEqual(get_valid_https_verify(True), True)
 
     def test_valid_https_verify_string_true(self):
         """Test return bool True when getting string True"""
-        self.assertEqual(utilconfig.get_valid_https_verify('True'), True)
+        self.assertEqual(get_valid_https_verify('True'), True)
 
     def test_valid_https_verify_string_false(self):
         """Test return bool False when getting string False"""
-        self.assertEqual(utilconfig.get_valid_https_verify('False'), False)
+        self.assertEqual(get_valid_https_verify('False'), False)
 
     def test_valid_https_verify_bool_string(self):
         """Test return string when getting a string value in https_verify"""
-        self.assertEqual(
-            utilconfig.get_valid_https_verify('ca_cert.crt'),
-            'ca_cert.crt'
-        )
+        self.assertEqual(get_valid_https_verify('ca_cert.crt'), 'ca_cert.crt')
