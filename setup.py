8b0c6b77bf46ec01b7b1f5464a8db0b8bc0acde9
Nicolas Liampotis <nliam@grnet.gr>
2016-07-19 10:52:00 +0300

    Run unit tests through `setup.py test`
    Closes #95

--- setup.py
+++ setup.py
@@ -1,8 +1,26 @@
-from setuptools import setup, find_packages
 import codecs
 import os
 import re
 import sys
+
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+
+class NoseTestCommand(TestCommand):
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import nose
+
+        test_script = os.path.join('b2handle', 'tests', 'main_test_script.py')
+        # See also nosetests section in setup.cfg
+        nose.run_exit(argv=['nosetests', test_script])
 
 
 # Set common test dependencies
@@ -14,6 +32,12 @@ test_dependencies = [
 if sys.version_info < (2, 7):
     test_dependencies.append('argparse')
     test_dependencies.append('unittest2')
+    # Workaround for atexit._run_exitfuncs error when invoking `test` with
+    # older versions of Python
+    try:
+        import multiprocessing
+    except ImportError:
+        pass
 
 
 here = os.path.abspath(os.path.dirname(__file__))
@@ -68,5 +92,5 @@ setup(name='b2handle',
           'datetime',
       ],
       tests_require=test_dependencies,
-      test_suite="nose.collector",
+      cmdclass={'test': NoseTestCommand},
 )
