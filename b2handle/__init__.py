95fdd53dc0f632fa42a2268d44635296b674063b
Merret Buurman <merretbuurman@users.noreply.github.com>
2016-07-21 16:35:36 +0200

    Cleaned help(b2handle) output. (#104)
    The comments inside the init.py were interpreted as docstrings and got printed in the help(b2handle) output, so I moved them to below the code.
    Things like Author, Author Email, Long Description are still missing from the help(b2handle) output.

--- b2handle/__init__.py
+++ b2handle/__init__.py
@@ -1,3 +1,11 @@
-#
-# The version as used in setup.py and docs/source/conf.py
+
 __version__ = "1.0.3"
+
+# The version as used in setup.py and docs/source/conf.py.
+
+# IMPORTANT
+# Please put no comment above the __version__ variable,
+# as they would be printed in the help(b2handle) output!
+# (The first comment - empty or not - would be appended to 
+# the package name, the next non-empty comment would be
+# printed as description).
