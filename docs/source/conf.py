c4f9655604dfc086f774c8f36d67e1cb7a639dc3
Tobias Weigel <weigel@dkrz.de>
2017-07-19 10:28:32 +0200

    switched docs theme to enable Sphinx upgrade

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -53,7 +53,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'B2Handle'
-copyright = u'2015-2016, Deutsches Klimarechenzentrum GmbH, GRNET S.A., SURFsara'
+copyright = u'2015-2017, Deutsches Klimarechenzentrum GmbH, GRNET S.A., SURFsara'
 author = u''
 
 # The version info for the project you're documenting, acts as replacement for
@@ -117,7 +117,7 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'bizstyle'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
